93cf9909301dea84c0af51f5645c69c375e6692b
Martin Czygan <martin.czygan@gmail.com>
2019-09-27 15:11:37 +0200

    run times

--- x/starvation/main.go
+++ x/starvation/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+// Q: Take a guess who might be faster, and by how much.
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	wg         sync.WaitGroup
+	sharedLock sync.Mutex
+)
+
+const runtime = 2 * time.Second
 
 func main() {
-	fmt.Println("vim-go")
+	greedyWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(3 * time.Nanosecond)
+			sharedLock.Unlock()
+			count++
+		}
+
+		fmt.Printf("greedy worker ran %v times\n", count)
+	}
+
+	politeWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now(); time.Since(begin) <= runtime; {
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+			count++
+		}
+
+		fmt.Printf("polite worker ran %v times\n", count)
+	}
+
+	wg.Add(2)
+	go greedyWorker()
+	go politeWorker()
+	wg.Wait()
 }
+
+// polite worker ran 169740 times
+// greedy worker ran 326774 times
